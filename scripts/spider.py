0c98b77b220ae0e3d3dc1801418ecf618a3fc08c
BrendonKootz <brendonkootz@gmail.com>
2019-03-06 21:05:05 -0600

    Added connection to google.com || Cleaned up uploading folder

--- scripts/spider.py
+++ scripts/spider.py
@@ -14,14 +14,14 @@ from datetime import datetime as DATE
 
 # Core definitions
 Item = "Lawn-Mower" # Psudeo variable for what is being searched
-clean_item = lambda x : x.replace(' ','-')
+clean_item = lambda x : x.replace(' ','-') # remove spaces, replace with a dash
 start_page = "https://www.google.com/utah-{}".format(clean_item(Item))
+page_response = requests.get(start_page, timeout=10)
+page_content = BS4(page_response.content,"html.parser")
 
 
 # Predefined list for all items found
 Items = list()
-
-
 
 # Connect to DB
 
