2e18016fa3d7a307599e1e597e51a75465e5dfd9
BrendonKootz <brendonkootz@gmail.com>
2019-03-11 18:15:24 -0500

    Spider test case skeleton built

---
+++ scripts/spider.py
@@ -0,0 +1,54 @@
+"""
+KSL-Alerts search spider
+Procedure:
+    - Do a google search
+    - Filter for Utah only listings
+    - Parse for the input item
+"""
+
+from bs4 import BeautifulSoup as BS4
+import requests
+from datetime import datetime as DATE
+
+# Core definitions
+Item = "Lawn-Mower" # Psudeo variable for what is being searched
+clean_item = lambda x : x.replace(' ','%20') # remove spaces, replace with a dash
+#start_page = "https://www.google.com/utah-{}".format(clean_item(Item))
+# Craigslist listing searches
+# Salt Lake City
+craig_page_SLC = "https://saltlakecity.craigslist.org/search/sss?query={}&sort=rel".format(clean_item(Item))
+
+# Logan
+craig_page_Logan = "https://logan.craigslist.org/search/sss?query={}&sort=rel".format(clean_item(Item))
+
+# Ogden
+craig_page_Ogden = "https://ogden.craigslist.org/search/sss?query={}&sort=rel".format(clean_item(Item))
+
+# Provo
+craig_page_Provo = "https://provo.craigslist.org/search/sss?query={}&sort=rel".format(clean_item(Item))
+
+# St George
+craig_page_StGeo = "https://stgeorge.craigslist.org/search/sss?query={}&sort=rel".format(clean_item(Item))
+
+# Responses
+#page_response = requests.get(start_page, timeout=10)
+response_SLC = requests.get(craig_page_SLC, timeout=10)
+response_Logan = requests.get(craig_page_Logan, timeout=10)
+response_Odgen = requests.get(craig_page_Ogden, timeout=10)
+response_Provo = requests.get(craig_page_Provo, timeout=10)
+response_StGeo = requests.get(craig_page_StGeo, timeout=10)
+
+# Render Content
+#page_content = BS4(page_response.content,"html.parser")
+content_SLC = BS4(response_SLC.content, "html.parser")
+content_Logan = BS4(response_Logan.content, "html.parser")
+content_Odgen = BS4(response_Odgen.content, "html.parser")
+content_Provo = BS4(response_Provo.content, "html.parser")
+content_StGeo = BS4(response_StGeo.content, "html.parser")
+
+# Predefined list for all items found
+Items = list()
+
+# Connect to DB
+
+print("TEST")
---
+++ scripts/spider.test.py
@@ -0,0 +1,25 @@
+import spider.py
+import unittest
+
+
+class TestSpider(unittest.TestCase):
+	""" Test class for the spider
+		- Test that link responses work
+		- Test for Item content
+		- Test for more..
+	"""
+		
+	def setUp(self):
+		pass
+
+	def test_link_not_null(self):
+		#self.assertEqual( spider web response test)
+		pass
+
+	def test_item_content(self):
+		#self.assertEqual( item stuff )
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
