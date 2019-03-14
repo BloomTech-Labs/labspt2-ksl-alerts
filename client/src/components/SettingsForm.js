84f00b29fc28e128de8b05cdf272c1522911ee6e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-03-13 22:04:23 -0400

    added SettingsForm.js

---
+++ client/src/components/SettingsForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label } from 'reactstrap';
+
+class SettingsForm extends Component {
+
+  render () {
+    return (
+
+    );
+  }
+}
