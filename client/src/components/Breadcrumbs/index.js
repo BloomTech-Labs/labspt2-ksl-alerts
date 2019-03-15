d5ce773799c4a801d61f967d4e399e6e7df80894
Elzy <belzy@rkdixon.com>
2019-03-14 21:10:41 -0500

    Brandon G. Elzy - Created breadcrumbs component

---
+++ client/src/components/Breadcrumbs/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Breadcrumbs = (props) {
+  return (
+    <div>
+    </div>
+  );
+}
+
+Breadcrumbs.propTypes = {
+  sections: PropTypes.arrayOf(PropTypes.shape({
+    
+  })),
+};
