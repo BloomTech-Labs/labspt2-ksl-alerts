6c4a0c42ddd61a02ac745a12b4dee6c7eb145d34
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-03-16 14:26:28 -0400

    added semantic-ui button, grid and header components created from for email and passwords

--- client/src/components/SettingsForm.js
+++ client/src/components/SettingsForm.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+import { Button, Form, Grid, Header } from "semantic-ui-react";
 
 const SettingsForm = () => (
-  <Form>
-    <Form.Group>
-      <Form.Field label="Email" control="input" />
-      <Form.Field label="Old Password" control="input" type="password" />
-      <Form.Field label="New Password" control="input" type="password" />
-    </Form.Group>
-  </Form>
+  <Grid columns={4}>
+    <Grid.Column>
+      <Header as="h2">Settings</Header>
+      <Form>
+        <Form.Field label="Email" control="input" />
+        <Form.Field label="Old Password" control="input" type="password" />
+        <Form.Field label="New Password" control="input" type="password" />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Grid.Column>
+  </Grid>
 );
 
 export default SettingsForm;
