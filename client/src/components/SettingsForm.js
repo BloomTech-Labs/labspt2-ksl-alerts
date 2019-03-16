4d07862c622eecf5d766b76eef2ca13483d6947c
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-03-16 15:14:38 -0400

    made component a class and defined state added handleInputChange method to update state of inputs added value and onChange properties to form inputs

--- client/src/components/SettingsForm.js
+++ client/src/components/SettingsForm.js
@@ -1,19 +1,66 @@
 import React, { Component } from "react";
-import { Button, Form, Grid, Header } from "semantic-ui-react";
+import { Button, Form, Grid, Header, Input } from "semantic-ui-react";
 
-const SettingsForm = () => (
-  <Grid columns={4}>
-    <Grid.Column>
-      <Header as="h2">Settings</Header>
-      <Form>
-        <Form.Field label="Email" control="input" />
-        <Form.Field label="Old Password" control="input" type="password" />
-        <Form.Field label="New Password" control="input" type="password" />
-        <Form.Field label="Phone Number" control="input" />
-        <Button type="submit">Submit</Button>
-      </Form>
-    </Grid.Column>
-  </Grid>
-);
+class SettingsForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      oldPassword: "",
+      newPassword: "",
+      phone: ""
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    // will need to an ajax post to update user settings
+  };
+
+  render() {
+    return (
+      <Grid columns={4}>
+        <Grid.Column>
+          <Header as="h2">Settings</Header>
+          <Form>
+            <Form.Input
+              label="Email"
+              control="input"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+            <Form.Input
+              label="Old Password"
+              control="input"
+              type="password"
+              value={this.state.oldPassword}
+              onChange={this.handleInputChange}
+            />
+            <Form.Input
+              label="New Password"
+              control="input"
+              type="password"
+              value={this.state.newPassword}
+              onChange={this.handleInputChange}
+            />
+            <Form.Input
+              label="Phone Number"
+              control="input"
+              value={this.state.phone}
+              onChange={this.handleInputChange}
+            />
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default SettingsForm;
