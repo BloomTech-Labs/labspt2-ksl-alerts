b58ecac8f73fab9c3b819c11a1ea99fbdc6b5600
Elzy <belzy@rkdixon.com>
2019-03-18 13:02:10 -0500

    Brandon G. Elzy - Added form fields to SignIn and SignUp components

--- client/src/components/presentation/SignInForm/SignInForm.js
+++ client/src/components/presentation/SignInForm/SignInForm.js
@@ -3,10 +3,65 @@ import { Form, } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const FormLayerOne = props => {
+
+  return (
+    <Form.Group>
+
+      <FieldContainer>
+        <Form.Input
+          label='Username'
+          placeholder='Username'
+        />
+      </FieldContainer>
+
+    </Form.Group>
+  );
+}
+
+const FormLayerTwo = props => {
+
+  return (
+    <Form.Group>
+
+      <FieldContainer>
+        <Form.Input
+          label='Password'
+          placeholder='Password'
+        />
+      </FieldContainer>
+
+    </Form.Group>
+  );
+}
+
+const FormLayerThree = props => {
+
+  return (
+    <Form.Group>
+
+      <FieldContainer>
+        <Form.Button
+
+        >
+          Sign In
+        </Form.Button>
+      </FieldContainer>
+
+    </Form.Group>
+  );
+}
+
 const SignInForm = props => {
   return (
     <Form as={ FormContainer }>
-      Sign In
+
+      <FormLayerOne />
+
+      <FormLayerTwo />
+
+      <FormLayerThree />
+
     </Form>
   );
 }
@@ -19,6 +74,18 @@ export default SignInForm;
 
 const FormContainer = styled.form`
 
-  border: 1px solid black;
+  /* border: 1px solid black; */
+  align-self: center;
+
+`;
+
+const FieldContainer = styled.div`
+
+  width: 200px;
+  margin-left: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  min-height: 65px;
 
 `;
--- client/src/components/presentation/SignUpForm/SignUpForm.js
+++ client/src/components/presentation/SignUpForm/SignUpForm.js
@@ -3,10 +3,82 @@ import { Form, } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+const FormLayerOne = props => {
+
+  return (
+    <Form.Group>
+
+      <FieldContainer>
+        <Form.Input
+          fluid
+          label='Username'
+          placeholder='Username'
+          type='text'
+        />
+      </FieldContainer>
+
+      <FieldContainer>
+        <Form.Input
+          fluid
+          label='Email'
+          placeholder='Email'
+          type='email'
+        />
+      </FieldContainer>
+
+    </Form.Group>
+  );
+}
+
+const FormLayerTwo = props => {
+
+  return (
+    <Form.Group>
+      <FieldContainer>
+        <Form.Input
+          fluid
+          label='Password'
+          placeholder='Password'
+          type='password'
+        />
+      </FieldContainer>
+
+      <FieldContainer>
+        <Form.Input
+          fluid
+          placeholder='Re-Type Password'
+          type='password'
+        />
+      </FieldContainer>
+    </Form.Group>
+  );
+}
+
+const FormLayerThree = props => {
+
+  return (
+    <Form.Group>
+      <FieldContainer>
+        <Form.Button
+
+        >
+          Sign Up
+        </Form.Button>
+      </FieldContainer>
+    </Form.Group>
+  );
+}
+
 const SignUpForm = props => {
   return (
     <Form as={ FormContainer }>
-      Sign Up
+
+      <FormLayerOne />
+
+      <FormLayerTwo />
+
+      <FormLayerThree />
+
     </Form>
   );
 }
@@ -19,6 +91,26 @@ export default SignUpForm;
 
 const FormContainer = styled.form`
 
-  border: 1px solid black;
+  /* border: 1px solid black; */ 
+  /* max-width: 600px; */
+  align-self: center;
+
+`;
+
+const Container = styled.div`
+
+  display: flex;
+  flex-direction: row;
+
+`;
+
+const FieldContainer = styled.div`
+
+  width: 200px;
+  margin-left: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  min-height: 65px;
 
 `;
