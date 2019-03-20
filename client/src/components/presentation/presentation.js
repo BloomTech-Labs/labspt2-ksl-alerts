d7753fb937c8b5591f619438dcff86ca27673d04
Lee Lichtsinn <leelichtsinn@Lees-MacBook-Pro.local>
2019-03-20 00:06:01 -0400

    exported Settings

--- client/src/components/presentation/presentation.js
+++ client/src/components/presentation/presentation.js
@@ -1,3 +1,4 @@
 export { default as CreateAlert     } from './CreateAlert/CreateAlert.js';
+export { default as Settings        } from './Settings/Settings.js';
 export { default as Topbar          } from './Topbar/Topbar.js';
 export { default as VerticalSidebar } from './VerticalSidebar/VerticalSidebar.js';
