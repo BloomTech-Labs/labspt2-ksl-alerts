26361296a2e16a5e62909ee93b32f9905bef07fa
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-03-21 21:48:19 -0400

    refactored forms to handle state, and state change

--- client/src/components/presentation/Settings/Settings.js
+++ client/src/components/presentation/Settings/Settings.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 import { Button, Field, Form, Grid, Header, Input } from "semantic-ui-react";
 
 class Settings extends Component {
