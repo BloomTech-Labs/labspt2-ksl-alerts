d44834b848d33c0a14fec20f05458801e61f64d3
BrendonKootz <brendonkootz@gmail.com>
2019-03-28 21:07:27 -0500

    Added ForgotPassword folder && file

---
+++ client/src/components/presentation/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Form, Segment, Label, Input, Icon, Button, } from 'semantic-ui-react';
+import { style } from './style/inline.js';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+// Present Forgot screen
+
+// User inputs email & username
+
+// User gets sent an email
+
+// User follows email
+
+// Password reset screen
